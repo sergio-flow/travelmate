722420756641392148e111c4b2722478b16268f4
sergio-flow <sergio.finciuc@gmail.com>
2025-04-13 10:35:20 +0300

    Milan first

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -7,16 +7,17 @@ interface VideoBackgroundProps {
 
 const VideoBackground = ({ selectedDestination }: VideoBackgroundProps) => {
   // https://youtu.be/lz6lhCdmAgQ?si=DSFlbfmR_GtWFZez&t=118
-  let videoURL = "https://www.youtube.com/embed/lz6lhCdmAgQ?t=118&autoplay=1&controls=0&showinfo=0&rel=0&loop=1&mute=1";
+  // let videoURL = "https://www.youtube.com/embed/8FCkb7NZ7SI?t=118&autoplay=1&controls=0&showinfo=0&rel=0&loop=1&mute=1";
+  const videoURL = "https://www.youtube.com/embed/8FCkb7NZ7SI?t=118&autoplay=1&controls=0&showinfo=0&rel=0&loop=1&mute=1"
   
   // Change video based on destination
-  if (selectedDestination) {
-    if (selectedDestination === "Amsterdam") {
-      videoURL = "https://www.youtube.com/embed/yIzn6Q-eku8?start=31&autoplay=1&controls=0&showinfo=0&rel=0&loop=1&playlist=yIzn6Q-eku8&mute=1";
-    } else if (selectedDestination === "Paris") {
-      videoURL = "https://www.youtube.com/embed/9Nz3H9eSKdE?autoplay=1&controls=0&showinfo=0&rel=0&loop=1&playlist=9Nz3H9eSKdE&mute=1";
-    }
-  }
+  // if (selectedDestination) {
+  //   if (selectedDestination === "Amsterdam") {
+  //     videoURL = "https://www.youtube.com/embed/yIzn6Q-eku8?start=31&autoplay=1&controls=0&showinfo=0&rel=0&loop=1&playlist=yIzn6Q-eku8&mute=1";
+  //   } else if (selectedDestination === "Paris") {
+  //     videoURL = "https://www.youtube.com/embed/9Nz3H9eSKdE?autoplay=1&controls=0&showinfo=0&rel=0&loop=1&playlist=9Nz3H9eSKdE&mute=1";
+  //   }
+  // }
 
   return (
     <div className="video-background-container" style={{ backgroundColor: 'black' }}>
