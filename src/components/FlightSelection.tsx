4d100b18e1720a057fffb5e2454e24ed3cc71244
sergio-flow <sergio.finciuc@gmail.com>
2025-04-17 08:02:58 +0300

    Go for it

--- src/components/FlightSelection.tsx
+++ src/components/FlightSelection.tsx
@@ -158,7 +158,7 @@ const FlightSelection = ({ flights, onSelectFlight, origin, participants }: Flig
     <div className="w-full max-w-lg mx-auto">
       <div className="mb-8">
         <h2 className="text-4xl font-bold text-white text-center">
-          Zboruri sub &euro;100<br />din fiecare luna
+          Destinatii sub &euro;100<br />din fiecare luna
         </h2>
         {/* <p className="text-white/80">
           {participants} traveler
